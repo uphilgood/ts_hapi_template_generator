91b7bbf134e36afe5fedd4c6410336adbdae0e88
philip.m.lee <philip.lee@greymatter.io>
2021-02-12 15:10:14 -0500

    remove gitignore files

--- dist/templates/typescript-hapi-project/build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-server_1.start();
--- dist/templates/typescript-hapi-project/src/util/config.ts
+++
@@ -1,8 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-export default {
-  PORT: process.env.PORT,
-  HOST: process.env.HOST,
-  BASE_URL: process.env.BASE_URL,
-  VERSION: process.env.VERSION,
-};
